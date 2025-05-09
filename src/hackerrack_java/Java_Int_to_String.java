290aec45353156de503217918e7b9d1af575deed
tzitziris <stzitzires@gmail.com>
2025-05-09 20:00:16 +0300

    Add files

---
+++ src/hackerrack_java/Java_Int_to_String.java
@@ -0,0 +1,14 @@
+package hackerrack_java;
+
+import java.util.Scanner;
+
+
+public class Java_Int_to_String {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Integer.toString(n);
+        System.out.print("Good job");
+    }
+}
